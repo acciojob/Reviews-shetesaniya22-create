05f9a77ed4adc1e2cd48252ee44c72fec1b949db
Saniya Shete <shetesaniya22@gmail.com>
2026-03-16 16:42:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,1 @@
-import React from "react";
-import "./../styles/App.css";
 
-function App() {
-  return (
-    <>
-      <p>I am learning React. My life is getting better.</p>
-    </>
-  );
-}
-
-export default App;
