45832c4f022c329140f35560cf7050e8715e171f
Saniya Shete <shetesaniya22@gmail.com>
2026-03-16 16:47:34 +0530

    Add React component with learning message

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,10 @@
+import './../styles/App.css';
+import React from "react";
 
+function App() {
+  return (
+    <p>I am learning React. My life is getting better.</p>
+  );
+}
+
+export default App;
